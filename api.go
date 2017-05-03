127b46c90d072c2308595e3a794e9e98554d2327
Pavel Busko <pavel-busko@users.noreply.github.com>
2017-05-03 18:48:20 +0300

    Update api.go

--- api.go
+++ api.go
@@ -55,7 +55,7 @@ func (response *ApiResponse) AddFile(file UploadedFile) {
 
 func init() {
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage:", filepath.Base(os.Args[0]), "/config/folder/path")
+		fmt.Fprintln(os.Stderr, "Usage:", filepath.Base(os.Args[0]), "/config/folder")
 		os.Exit(1)
 	}
 
